5740ee41f81c722aea5db4ca3d03d397aab02f59
Tri Tran <tri.trancongtoan1999@gmail.com>
2021-03-24 23:51:20 +0700

    minor changes to HUD

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ class GameManager:
         self.HOLECOLUMNS = 3  # !!
         self.HOLEWIDTH = 100
         self.HOLEHEIGHT = int(self.HOLEWIDTH * (3 / 8))
+        self.GRAVESTONE_WIDTH = int(self.HOLEWIDTH * (5 / 8))
+        self.GRAVESTONE_HEIGHT = int(self.GRAVESTONE_WIDTH * (6/5))
         self.ZOMBIE_WIDTH =int( self.HOLEWIDTH*(7/8) )
         self.ZOMBIE_HEIGHT = self.ZOMBIE_WIDTH
         self.FONT_SIZE = 31
-        self.FONT_TOP_MARGIN = 26
+        self.FONT_TOP_MARGIN = 35
         self.LEVEL_SCORE_GAP = 4
         self.LEFT_MOUSE_BUTTON = 1
         self.positions = []
-        self.GAME_TITLE = "Whack A Mole - Game Programming - Assignment 1"
+        self.GAME_TITLE = "Whack A Zombie - Game Programming - Assignment 1"
         # Initialize player's score, number of missed hits and level
         self.score = 0
         self.misses = 0
@@ -34,12 +36,14 @@ class GameManager:
         self.cursor_img = transform.scale(pygame.image.load("images/hammer.png"), (self.ZOMBIE_WIDTH, self.ZOMBIE_HEIGHT))
 
         # Font object for displaying text
-        self.font_obj = pygame.font.Font('./fonts/GROBOLD.ttf', self.FONT_SIZE)
+        self.font_obj = pygame.font.Font('./fonts/Foul Fiend.ttf', self.FONT_SIZE)
         # Initialize the zombie's sprite sheet
         # 6 different states
         sprite_sheet = pygame.image.load("images/zombie.png")
         zombie = pygame.image.load("images/zombie.png")
         zombie_hit = pygame.image.load("images/zombie_hit.png")
+        self.gravestone = pygame.image.load("images/gravestone.png")
+        self.gravestone = transform.scale(self.gravestone, (self.GRAVESTONE_WIDTH, self.GRAVESTONE_HEIGHT))
         zombie = transform.scale(zombie, (self.ZOMBIE_WIDTH, self.ZOMBIE_HEIGHT))
         zombie_hit = transform.scale(zombie_hit, (self.ZOMBIE_WIDTH, self.ZOMBIE_HEIGHT))
         self.zombie = []
@@ -56,7 +60,6 @@ class GameManager:
         self.reset()
 
     def reset(self):
-
         # Generate hole positions
         base_row = (self.SCREEN_HEIGHT - self.FONT_TOP_MARGIN*3) / self.HOLEROWS
         base_column = self.SCREEN_WIDTH / self.HOLECOLUMNS
@@ -96,7 +99,7 @@ class GameManager:
         mouse_x = mouse_position[0]
         mouse_y = mouse_position[1]
         current_hole_x = current_hole_position[0] + (self.HOLEWIDTH-self.ZOMBIE_WIDTH)/2
-        current_hole_y = current_hole_position[1]+self.HOLEHEIGHT-self.ZOMBIE_HEIGHT*1.2
+        current_hole_y = current_hole_position[1]+self.HOLEHEIGHT-self.ZOMBIE_HEIGHT
         if (mouse_x > current_hole_x) \
                 and (mouse_x < current_hole_x + self.ZOMBIE_WIDTH) \
                 and (mouse_y > current_hole_y) \
@@ -109,21 +112,21 @@ class GameManager:
     def update(self):
         # Update the player's score
         current_score_string = "SCORE: " + str(self.score)
-        score_text = self.font_obj.render(current_score_string, True, (255, 255, 255))
+        score_text = self.font_obj.render(current_score_string, True, (139, 0, 0))
         score_text_pos = score_text.get_rect()
         score_text_pos.centerx = self.background.get_rect().centerx
         score_text_pos.centery = self.FONT_TOP_MARGIN
         self.screen.blit(score_text, score_text_pos)
         # Update the player's misses
         current_misses_string = "MISSES: " + str(self.misses)
-        misses_text = self.font_obj.render(current_misses_string, True, (255, 255, 255))
+        misses_text = self.font_obj.render(current_misses_string, True, (139, 0, 0))
         misses_text_pos = misses_text.get_rect()
         misses_text_pos.centerx = self.SCREEN_WIDTH / 5 * 4
         misses_text_pos.centery = self.FONT_TOP_MARGIN
         self.screen.blit(misses_text, misses_text_pos)
         # Update the player's level
         current_level_string = "LEVEL: " + str(self.level)
-        level_text = self.font_obj.render(current_level_string, True, (255, 255, 255))
+        level_text = self.font_obj.render(current_level_string, True, (139, 0, 0))
         level_text_pos = level_text.get_rect()
         level_text_pos.centerx = self.SCREEN_WIDTH / 5 * 1
         level_text_pos.centery = self.FONT_TOP_MARGIN
@@ -138,18 +141,13 @@ class GameManager:
         is_down = False
         interval = 1
         initial_interval = 1
-        hit_interval = 0.2
+        hit_interval = 0.15
         hit_loop = 0
         frame_num = 0
         # Time control variables
         clock = pygame.time.Clock()
 
-        for i in range(len(self.zombie)):
-            self.zombie[i].set_colorkey((0, 0, 0))
-            self.zombie[i] = self.zombie[i].convert_alpha()
-
         while loop:
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     loop = False
@@ -178,6 +176,7 @@ class GameManager:
                 self.screen.blit(self.background, (0, 0))
                 for position in self.hole_positions:
                     self.screen.blit(self.img_hole, position)
+                    self.screen.blit(self.gravestone, position)
                 self.update()
                 num = -1
             if num == -1 and (hit_loop > 2 or hit_loop == 0):
@@ -185,13 +184,13 @@ class GameManager:
                 self.screen.blit(self.background, (0, 0))
                 for position in self.hole_positions:
                     self.screen.blit(self.img_hole, position)
+                    self.screen.blit(self.gravestone, position)
                 self.update()
                 num = 0
                 is_down = False
-                interval = 0.5
+                interval = 0.3
                 frame_num = random.randint(0, self.HOLEROWS*self.HOLECOLUMNS-1)
                 self.soundEffect.playPop()
-                print(frame_num)
             mil = clock.tick(self.FPS)
             sec = mil / 1000.0
             cycle_time += sec
@@ -200,16 +199,15 @@ class GameManager:
                 self.screen.blit(self.background, (0, 0))
                 for position in self.hole_positions:
                     self.screen.blit(self.img_hole, position)
+                    self.screen.blit(self.gravestone, (position[0] + self.HOLEWIDTH/2, position[1] - self.HOLEHEIGHT/2))
                 self.screen.blit(pic, (self.hole_positions[frame_num][0]+(self.HOLEWIDTH-self.ZOMBIE_WIDTH)/2,
                                        self.hole_positions[frame_num][1]+self.HOLEHEIGHT-self.ZOMBIE_HEIGHT*1.2))
-
                 self.update()
                 if hit_loop ==0:
                     if is_down is False:
                         num += 1
                     else:
                         num -= 1
-
                 if num == 1:
                     num -= 1
                     if hit_loop > 2:
@@ -222,14 +220,14 @@ class GameManager:
                     else:
                         interval = self.get_interval_by_level(initial_interval)  # get the newly decreased interval value
                     is_down = True
-
                 else:
-                    interval = 0.5
+                    interval = 0.3
                 cycle_time = 0
 
             self.screen.blit(self.background, (0, 0))
             for position in self.hole_positions:
                 self.screen.blit(self.img_hole, position)
+                self.screen.blit(self.gravestone, (position[0] + self.HOLEWIDTH / (16/3), position[1] - self.HOLEHEIGHT*1.8))
             self.screen.blit(pic, (self.hole_positions[frame_num][0]+(self.HOLEWIDTH-self.ZOMBIE_WIDTH)/2,
                                        self.hole_positions[frame_num][1]+self.HOLEHEIGHT-self.ZOMBIE_HEIGHT*1.2))
             self.cursor_img_rect.midleft = pygame.mouse.get_pos()  # update position
@@ -255,21 +253,21 @@ class GameManager:
             score_text = self.font_obj.render(current_score_string, True, (255, 255, 255))
             score_text_pos = score_text.get_rect()
             score_text_pos.centerx = self.background.get_rect().centerx
-            score_text_pos.centery = self.FONT_TOP_MARGIN*10
+            score_text_pos.centery = self.FONT_TOP_MARGIN*5
             self.screen.blit(score_text, score_text_pos)
             # Update the player's misses
             current_misses_string = "MISSES: " + str(self.misses)
             misses_text = self.font_obj.render(current_misses_string, True, (255, 255, 255))
             misses_text_pos = misses_text.get_rect()
             misses_text_pos.centerx = self.background.get_rect().centerx
-            misses_text_pos.centery = self.FONT_TOP_MARGIN*12
+            misses_text_pos.centery = self.FONT_TOP_MARGIN*8
             self.screen.blit(misses_text, misses_text_pos)
             # Update the player's level
             current_level_string = "LEVEL: " + str(self.level)
             level_text = self.font_obj.render(current_level_string, True, (255, 255, 255))
             level_text_pos = level_text.get_rect()
             level_text_pos.centerx = self.background.get_rect().centerx
-            level_text_pos.centery = self.FONT_TOP_MARGIN*14
+            level_text_pos.centery = self.FONT_TOP_MARGIN*11
             self.screen.blit(level_text, level_text_pos)
             
             # Game Over string
@@ -277,7 +275,7 @@ class GameManager:
             gameover_text = self.font_obj.render(gameover_string, True, (255, 255, 255))
             gameover_text_pos = gameover_text.get_rect()
             gameover_text_pos.centerx = self.background.get_rect().centerx
-            gameover_text_pos.centery = self.FONT_TOP_MARGIN*5
+            gameover_text_pos.centery = self.FONT_TOP_MARGIN*2
             self.screen.blit(gameover_text, gameover_text_pos)
 
 
@@ -286,7 +284,7 @@ class GameManager:
             continue_text = self.font_obj.render(continue_string, True, (255, 255, 255))
             continue_text_pos = continue_text.get_rect()
             continue_text_pos.centerx = self.background.get_rect().centerx
-            continue_text_pos.centery = self.FONT_TOP_MARGIN*20
+            continue_text_pos.centery = self.FONT_TOP_MARGIN*15
             self.screen.blit(continue_text, continue_text_pos)
         
 
@@ -296,12 +294,6 @@ class GameManager:
         self.misses = 0
         self.level = 1
         
-
-
-
-
-# The Debugger class - use this class for printing out debugging information
-
 
 
 class SoundEffect:
